61778f363b57cf36ffb6cbccc49c675ada6fb87c
Hans Köpp <hanskoppvi@gmail.com>
2022-03-06 12:38:54 +0100

    Add colors and strucs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,68 @@
-use std::io;
+extern crate termion;
 
-type Row = [char; 5];
+use std::io;
+use termion::color;
+
+#[derive(Copy, Clone)]
+enum CharCond {
+    None,
+    Right,
+    Missplaced,
+}
+
+#[derive(Copy, Clone)]
+struct CharElement {
+    character: char,
+    cond: CharCond,
+}
+type Row = [CharElement; 5];
 type Board = [Row; 2];
 
 fn print_board(board: Board) {
+    println!();
     for col in board {
-        print!("|");
+        print!(
+            "       {}|{}",
+            color::Bg(color::Rgb(0x2c, 0x30, 0x32)),
+            color::Bg(color::Reset)
+        );
         for square in col {
-            print!("{}|", square);
+            match square.cond {
+                CharCond::None => print!(
+                    "{}{}{}{}{}{}|{}",
+                    color::Bg(color::Rgb(0x2c, 0x30, 0x32)),
+                    termion::style::Bold,
+                    color::Fg(color::Rgb(0xe8, 0xe6, 0xe3)),
+                    square.character,
+                    color::Fg(color::Reset),
+                    color::Bg(color::Rgb(0x2c, 0x30, 0x32)),
+                    color::Bg(color::Reset),
+                ),
+                CharCond::Missplaced => print!(
+                    "{}{}{}{}{}{}|{}",
+                    color::Bg(color::Rgb(0x91, 0x7f, 0x2f)),
+                    termion::style::Bold,
+                    color::Fg(color::Rgb(0xe8, 0xe6, 0xe3)),
+                    square.character,
+                    color::Fg(color::Reset),
+                    color::Bg(color::Rgb(0x2c, 0x30, 0x32)),
+                    color::Bg(color::Reset),
+                ),
+                CharCond::Right => print!(
+                    "{}{}{}{}{}{}|{}",
+                    color::Bg(color::Rgb(0x42, 0x71, 0x3e)),
+                    termion::style::Bold,
+                    color::Fg(color::Rgb(0xe8, 0xe6, 0xe3)),
+                    square.character,
+                    color::Fg(color::Reset),
+                    color::Bg(color::Rgb(0x2c, 0x30, 0x32)),
+                    color::Bg(color::Reset),
+                ),
+            }
         }
         println!();
     }
+    println!();
 }
 
 /**
@@ -23,23 +75,31 @@ fn print_board(board: Board) {
 fn main() {
     let mut input = String::new();
     let game_word = String::from("decay");
-    let mut board: Board = [[' '; 5]; 2];
+    let mut board: Board = [[CharElement {
+        character: ' ',
+        cond: CharCond::None,
+    }; 5]; 2];
 
-    for i in 0..2 {
+    println!();
+    println!("         Wordle!");
+
+    for current_row in 0..board.len() {
         print_board(board);
-        println!("Enter your guess");
+        println!("Enter your guess:");
         input.clear();
 
         io::stdin().read_line(&mut input).unwrap();
         input.pop(); // remove enter
         if input.len() == 5 {
-            for (j, c) in input.to_string().chars().enumerate() {
-                if c == game_word.chars().nth(j).unwrap() {
-                    board[i][j] = '1';
-                } else if game_word.chars().position(|d| d == c) != None {
-                    board[i][j] = '0';
-                } else {
-                    board[i][j] = c;
+            for (current_col, input_char) in input.to_string().chars().enumerate() {
+                board[current_row][current_col].character = input_char;
+                // Is the current char from the input in this column the same as in the word.
+                if input_char == game_word.chars().nth(current_col).unwrap() {
+                    board[current_row][current_col].cond = CharCond::Right;
+                }
+                // Is the current char from the input in the word.
+                else if game_word.chars().position(|d| d == input_char) != None {
+                    board[current_row][current_col].cond = CharCond::Missplaced;
                 }
             }
         }
